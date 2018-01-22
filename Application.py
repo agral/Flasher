7d998b4681306829afa2f201672bd0bd26a0e3d1
aszczerbiak <szczerbiakadam@gmail.com>
2018-01-22 15:50:59 +0100

    Add sourcedata-selection groupbox to the GUI
    Adds a group-box that allows the user to select/change the source data.
    It has not been assigned any logic yet.

--- Application.py
+++ Application.py
@@ -8,6 +8,17 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        self.lf_sourcedata = tk.LabelFrame(self, text="Source data:",
+                padx=5, pady=5)
+        self.lf_sourcedata.pack(padx=5, pady=5)
+
+        self.lbl_sourcedata = tk.Label(
+                self.lf_sourcedata, text="(none)")
+        self.lbl_sourcedata.pack(side="left")
+        self.btn_setsourcedata = tk.Button(
+                self.lf_sourcedata, text="Change")
+        self.btn_setsourcedata.pack(side="right")
+
         self.btn_quit = tk.Button(
                 self, text="EXIT", command=self.master.destroy)
         self.btn_quit.pack(side="bottom")
