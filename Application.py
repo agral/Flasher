cd92d1cbeaae6f9c31f3f0b89dba41c3ba16c528
aszczerbiak <szczerbiakadam@gmail.com>
2018-01-23 14:14:36 +0100

    Add possibility to select any output directory

--- Application.py
+++ Application.py
@@ -7,7 +7,10 @@ from Config import Config
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+
         self.sourcedata_path = None
+        self.outfile_path = Config.DIR_OUTPUT_DEFAULT
+
         master.minsize(
                 width=Config.MAIN_WINDOW_WIDTH,
                 height=Config.MAIN_WINDOW_HEIGHT)
@@ -19,7 +22,6 @@ class Application(tk.Frame):
         self.lf_sourcedata = tk.LabelFrame(self, text="Source data:",
                 padx=5, pady=5)
         self.lf_sourcedata.pack(padx=5, pady=5, fill="x")
-
         self.lbl_sourcedata = tk.Label(
                 self.lf_sourcedata, text="(none)")
         self.lbl_sourcedata.pack(side="left")
@@ -27,6 +29,17 @@ class Application(tk.Frame):
                 self.lf_sourcedata, text="Change",
                 command=self.invoke_sourcedata_dialog)
         self.btn_setsourcedata.pack(side="right")
+
+        self.lf_outputdir = tk.LabelFrame(self, text="Output directory:",
+                padx=5, pady=5)
+        self.lf_outputdir.pack(padx=5, pady=5, fill="x")
+        self.lbl_outputdir = tk.Label(
+                self.lf_outputdir, text=self.outfile_path)
+        self.lbl_outputdir.pack(side="left")
+        self.btn_setoutputdir = tk.Button(
+                self.lf_outputdir, text="Change",
+                command=self.invoke_setoutputdir_dialog)
+        self.btn_setoutputdir.pack(side="right")
 
         self.btn_quit = tk.Button(
                 self, text="EXIT", command=self.master.destroy)
@@ -41,3 +54,9 @@ class Application(tk.Frame):
         )
         self.lbl_sourcedata["text"] = self.sourcedata_path or "(none)"
 
+
+    def invoke_setoutputdir_dialog(self):
+        self.outfile_path = tk.filedialog.askdirectory(
+                initialdir = self.outfile_path,
+                title = "Select output directory")
+        self.lbl_outputdir["text"] = self.outfile_path or "(none)"
--- Config.py
+++ Config.py
@@ -6,5 +6,8 @@ class Config:
     MAIN_WINDOW_HEIGHT = 600
     DIR_HOME = os.path.expanduser("~")
     DIR_APP_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+    DEFAULT_PDF_FILENAME = "output.pdf"
+    DIR_OUTPUT_DEFAULT = os.path.join(DIR_APP_ROOT, "out")
     def __init__(self):
         pass
