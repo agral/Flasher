ed5f6e41e520a861204b50b0a010e07560ffb8a4
aszczerbiak <szczerbiakadam@gmail.com>
2018-01-23 10:28:07 +0100

    Make the "Sourcedata-change" button operational
    Adds a callback that handles the clicking of the "Change" button in the
    source data selection section.

--- Application.py
+++ Application.py
@@ -1,11 +1,16 @@
 import tkinter as tk
+from tkinter import filedialog
+
+from Config import Config
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.sourcedata_path = None
         self.pack()
         self.create_widgets()
+
 
     def create_widgets(self):
         self.lf_sourcedata = tk.LabelFrame(self, text="Source data:",
@@ -16,11 +21,20 @@ class Application(tk.Frame):
                 self.lf_sourcedata, text="(none)")
         self.lbl_sourcedata.pack(side="left")
         self.btn_setsourcedata = tk.Button(
-                self.lf_sourcedata, text="Change")
+                self.lf_sourcedata, text="Change",
+                command=self.invoke_sourcedata_dialog)
         self.btn_setsourcedata.pack(side="right")
 
         self.btn_quit = tk.Button(
                 self, text="EXIT", command=self.master.destroy)
         self.btn_quit.pack(side="bottom")
 
+
+    def invoke_sourcedata_dialog(self):
+        self.sourcedata_path = tk.filedialog.askopenfilename(
+                initialdir = Config.DIR_HOME,
+                title = "Select source data file",
+                filetypes = (("CSV files", "*.csv"), ("All files", "*.*"))
+        )
+        self.lbl_sourcedata["text"] = self.sourcedata_path or "(none)"
 
