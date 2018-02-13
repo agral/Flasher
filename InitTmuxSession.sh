157644ce079be2b60fe158f584735bc6df889b77
aszczerbiak <szczerbiakadam@gmail.com>
2018-02-13 17:35:07 +0100

    Add sanity check for no Flasher session present

--- InitTmuxSession.sh
+++ InitTmuxSession.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
 SCRIPT_NAME="$(basename "${0}")"
+SESSION_NAME="Flasher"
 
 
 printf "%s\n" "Self-checking..."
 
-# Checks whether tmux server exists:
 printf " --> %s " "Checking whether tmux server exists..."
-if [ ! tmux info &>/dev/null ] ; then
+if [ ! tmux info &>/dev/null ]; then
   printf "%s\n" "NO."
   exit 1
 else
   printf "%s\n" "yes."
 fi
+
+printf " --> %s \"%s\" %s " "Checking whether session" "Flasher" "exists..."
+if [ tmux has-session -t "${SESSION_NAME}" 2>/dev/null ]; then
+  printf "%s\n" "YES."
+  exit 1
+else
+  printf "%s\n" "no."
+fi
+
+printf "Self-check passed.\n"
