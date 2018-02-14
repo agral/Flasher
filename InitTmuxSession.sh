9cf4e2a650a729b8e85417ee3c98cb16f188bded
aszczerbiak <szczerbiakadam@gmail.com>
2018-02-14 22:51:54 +0100

    Make InitTmuxSession script create actual session

--- InitTmuxSession.sh
+++ InitTmuxSession.sh
@@ -47,11 +47,72 @@ printf " --> %s \"${colorC}%s${colorNONE}\" %s " \
     "Checking whether session" "Flasher" "exists..."
 if (tmux has-session -t "${SESSION_NAME}" 2>/dev/null); then
   printf "${colorR}%s${colorNONE}%s\n" "YES" "."
-  fail_self_test
+  #fail_self_test
+  tmux kill-session -t "${SESSION_NAME}" 2>/dev/null
 else
   printf "${colorG}%s${colorNONE}%s\n" "no" "."
 fi
 
 printf "%s ${colorG}%s${colorNONE}%s\n" "Self-check" "passed" "."
 
+printf "%s\n" "Creating session contents..."
 tmux new-session -d -s "${SESSION_NAME}"
+
+# First window: git. Left pane: status, right pane: log of commits.
+tmux rename-window -t "${SESSION_NAME}:1" "git"
+tmux send-keys -t "${SESSION_NAME}:1" "git st" Enter
+tmux split-window -t "${SESSION_NAME}:1" -h
+tmux send-keys -t "${SESSION_NAME}:1.right" "git lc" Enter
+tmux select-pane -t "${SESSION_NAME}:1.left"
+printf " --> [${colorG}OK${colorNONE}] ${colorY}%s${colorNONE}" "1"
+printf ": ${colorM}%s${colorNONE}\n" "git"
+
+# Second window: main code. Left pane: flake, right pane: vim.
+tmux new-window -t "${SESSION_NAME}:2" -n "main"
+tmux send-keys -t "${SESSION_NAME}:2" "flake8 main.py" Enter
+tmux split-window -t "${SESSION_NAME}:2" -h
+tmux send-keys -t "${SESSION_NAME}:2.right" "vim main.py" Enter
+tmux select-pane -t "${SESSION_NAME}:2.right"
+printf " --> [${colorG}OK${colorNONE}] ${colorY}%s${colorNONE}" "2"
+printf ": ${colorM}%s${colorNONE}\n" "main"
+
+# Third window: Application code. Left pane: flake, right pane: vim.
+tmux new-window -t "${SESSION_NAME}:3" -n "Application"
+tmux send-keys -t "${SESSION_NAME}:3" "flake8 Application.py" Enter
+tmux split-window -t "${SESSION_NAME}:3" -h
+tmux send-keys -t "${SESSION_NAME}:3.right" "vim Application.py" Enter
+tmux select-pane -t "${SESSION_NAME}:3.right"
+printf " --> [${colorG}OK${colorNONE}] ${colorY}%s${colorNONE}" "3"
+printf ": ${colorM}%s${colorNONE}\n" "Application"
+
+# Fourth window: LatexBuilder code. Left pane: flake, right pane: vim.
+tmux new-window -t "${SESSION_NAME}:4" -n "LatexBuilder"
+tmux send-keys -t "${SESSION_NAME}:4" "flake8 LatexBuilder.py" Enter
+tmux split-window -t "${SESSION_NAME}:4" -h
+tmux send-keys -t "${SESSION_NAME}:4.right" "vim LatexBuilder.py" Enter
+tmux select-pane -t "${SESSION_NAME}:4.right"
+printf " --> [${colorG}OK${colorNONE}] ${colorY}%s${colorNONE}" "4"
+printf ": ${colorM}%s${colorNONE}\n" "LatexBuilder"
+
+# Fifth window: Config code. Left pane: flake, right pane: vim.
+tmux new-window -t "${SESSION_NAME}:5" -n "Config"
+tmux send-keys -t "${SESSION_NAME}:5" "flake8 Config.py" Enter
+tmux split-window -t "${SESSION_NAME}:5" -h
+tmux send-keys -t "${SESSION_NAME}:5.right" "vim Config.py" Enter
+tmux select-pane -t "${SESSION_NAME}:5.right"
+printf " --> [${colorG}OK${colorNONE}] ${colorY}%s${colorNONE}" "5"
+printf ": ${colorM}%s${colorNONE}\n" "Config"
+
+# Sixth window: Utils code. Left pane: flake, right pane: vim.
+tmux new-window -t "${SESSION_NAME}:6" -n "Utils"
+tmux send-keys -t "${SESSION_NAME}:6" "flake8 Utils.py" Enter
+tmux split-window -t "${SESSION_NAME}:6" -h
+tmux send-keys -t "${SESSION_NAME}:6.right" "vim Utils.py" Enter
+tmux select-pane -t "${SESSION_NAME}:6.right"
+printf " --> [${colorG}OK${colorNONE}] ${colorY}%s${colorNONE}" "6"
+printf ": ${colorM}%s${colorNONE}\n" "Utils"
+
+# Brings first window (git) to front:
+tmux select-window -t "${SESSION_NAME}:1"
+
+printf "${colorG}%s${colorNONE}%s\n" "Successfully completed" "."
